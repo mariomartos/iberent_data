acd0a97dd058625c735e01de003fe841c393a5d6
mariomartos <mmartoma14@gmail.com>
2025-07-03 10:03:55 +0200

    connections fixed and now db is on query

--- test_connection.py
+++ test_connection.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from datetime import datetime
-from utilities.configuration_connection import oracle_connection
+from utilities.configuration_connection import connection_3411
 from utilities.yaml_reader import generate_user_input, generate_sql_script
 
 workspace = r"X:\iberent_data"
@@ -10,10 +10,10 @@ start_time = datetime.now()
 user_input = generate_user_input(workspace)
 
 # 2) Conexión a la BD de Salesforce
-ora_conn = oracle_connection(user_input)
+ora_conn = connection_3411(user_input)
 
 # 3) Probar la conexión
-df = pd.read_sql("SELECT * FROM IBERICA.BAILLEUR", ora_conn)
+df = pd.read_sql("SELECT TOP 10 * FROM [SGM].[dbo].[Brand]", ora_conn)
 print(df.head())
 
 ora_conn.close()
--- utilities/configuration_connection.py
+++ utilities/configuration_connection.py
@@ -1,65 +1,68 @@
+from datetime import datetime
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
 import oracledb
+import sqlparse
 
-def salesforce_connection(user_input: dict):
-    host     = user_input["salesforce_host"]
-    database = user_input["salesforce_database"]
-    driver   = user_input["salesforce_driver"]
-
-    extras = "&Encrypt=yes"
+def connection_3411(user_input):
+    tail = "&Encrypt=yes&Trusted_Connection=yes"
     if user_input.get("salesforce_trust_cert", "no").lower() == "yes":
-        extras += "&TrustServerCertificate=yes"
+        tail += "&TrustServerCertificate=yes"
 
-    # URL con Trusted_Connection=yes (Windows Authentication)
     conn_url = (
-        f"mssql+pyodbc://@{host}/{database}"
-        f"?driver={driver}{extras}&Trusted_Connection=yes"
-    )
+        f"mssql+pyodbc://@{user_input['salesforce_host']}/{user_input['salesforce_database']}"
+        f"?driver={quote_plus(user_input['salesforce_driver'])}{tail}")
 
-    engine = create_engine(conn_url, fast_executemany=True)
-    conn   = engine.connect()
-    print("✅ Connection SQL Server – Salesforce_DB")
+    conn = create_engine(conn_url, fast_executemany=True).connect()
+    print("✅ Conexión SQL Server 34.11")
     return conn
 
-def sgm_connection(user_input: dict):
-    host     = user_input["sgm_host"]
-    database = user_input["sgm_database"]
-    driver   = user_input["sgm_driver"]
-
-    extras = "&Encrypt=yes"
-    if user_input.get("sgm_trust_cert", "no").lower() == "yes":
-        extras += "&TrustServerCertificate=yes"
-
-    # URL con Trusted_Connection=yes (Windows Authentication)
-    conn_url = (
-        f"mssql+pyodbc://@{host}/{database}"
-        f"?driver={driver}{extras}&Trusted_Connection=yes"
-    )
-
-    engine = create_engine(conn_url, fast_executemany=True)
-    conn   = engine.connect()
-    print("✅ Connection SQL Server – SGM")
+def oracle_connection(user_input):
+    oracledb.init_oracle_client(lib_dir=user_input["oracle_lib_dir"])
+    
+    dsn = oracledb.makedsn( user_input["oracle_host"],                
+                            port = user_input.get("oracle_port"),                             
+                            sid=user_input["oracle_sid"])              
+    
+    conn = oracledb.connect(user = user_input["oracle_user"],
+                            password = user_input["oracle_password"],
+                            dsn = dsn)
+    print("✅ Oracle Connection")
     return conn
 
-def oracle_connection(cfg):
-    user     = cfg["oracle_user"]
-    password = cfg["oracle_password"]
-    host     = cfg["oracle_host"]
-    port     = cfg.get("oracle_port", 1521)
 
-    # --- Modo thick opcional -----------------------------------------
-    if cfg.get("oracle_mode", "thin").lower() == "thick":
-        oracledb.init_oracle_client(lib_dir=cfg.get("oracle_lib_dir"))
 
-    # --- Construir DSN según SID o Service ---------------------------
-    if "oracle_sid" in cfg:
-        dsn = oracledb.makedsn(host, port, sid=cfg["oracle_sid"])
-    elif "oracle_service" in cfg:
-        dsn = oracledb.makedsn(host, port, service_name=cfg["oracle_service"])
-    else:
-        raise KeyError("Debes definir 'oracle_sid' o 'oracle_service' en el YAML")
+#Functions to review
+def play_sql_script(workspace,sql_path,connection):
+    #Read file location
+    sql_file_path = f"{workspace}\\{sql_path}" 
+    sql_script = open(sql_file_path,'r')
+    script = sql_script.read()
+    script = sqlparse.format(script, strip_comments=True).strip()
+    statements = sqlparse.split(script)
 
-    conn = oracledb.connect(user=user, password=password, dsn=dsn)
-    print("✅ Connection Oracle –", dsn)
-    return conn
+    for sql in statements:
+        start_time = datetime.now()
+        connection.execute(sql)
+        end_time = datetime.now()
+        print('Query Executed - Duration: {}'.format(end_time - start_time))
+
+def play_sql_view_script(workspace,connection):
+    #Read file location
+    #Mango Views - master level 
+    workspace = workspace.replace('\\online_operations_project','')
+    sql_file_path = f"{workspace}\\mango_local_views.sql" 
+    sql_script = open(sql_file_path,'r')
+    script = sql_script.read()
+    script = sqlparse.format(script, strip_comments=True).strip()
+    statements = sqlparse.split(script)
+
+    start_time = datetime.now()
+    
+    for sql in statements:
+        #% Escape character issue - replace all the cases with 2 % instead of 1
+        sql = sql.replace('%','%%')
+        try:
+            connection.execute(sql)
+        except:
+            print('Error - View Query failed to update')   
