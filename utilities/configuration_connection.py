6d55c28cb9bc82a30cbf3a880397cfd2c9f83b9c
mariomartos <mmartoma14@gmail.com>
2025-07-03 10:09:19 +0200

    def play_sql_script

--- utilities/configuration_connection.py
+++ utilities/configuration_connection.py
@@ -30,9 +30,6 @@ def oracle_connection(user_input):
     print("✅ Oracle Connection")
     return conn
 
-
-
-#Functions to review
 def play_sql_script(workspace,sql_path,connection):
     #Read file location
     sql_file_path = f"{workspace}\\{sql_path}" 
@@ -46,23 +43,3 @@ def play_sql_script(workspace,sql_path,connection):
         connection.execute(sql)
         end_time = datetime.now()
         print('Query Executed - Duration: {}'.format(end_time - start_time))
-
-def play_sql_view_script(workspace,connection):
-    #Read file location
-    #Mango Views - master level 
-    workspace = workspace.replace('\\online_operations_project','')
-    sql_file_path = f"{workspace}\\mango_local_views.sql" 
-    sql_script = open(sql_file_path,'r')
-    script = sql_script.read()
-    script = sqlparse.format(script, strip_comments=True).strip()
-    statements = sqlparse.split(script)
-
-    start_time = datetime.now()
-    
-    for sql in statements:
-        #% Escape character issue - replace all the cases with 2 % instead of 1
-        sql = sql.replace('%','%%')
-        try:
-            connection.execute(sql)
-        except:
-            print('Error - View Query failed to update')   
